2b5a391f77a078757a91508c3451cb70a906d0a7
Elif KAYMAK <camlielif@gmail.com>
2023-12-12 09:18:42 +0300

    Contact sayfası eklendi.

---
+++ Frondends/RentCar.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentCar.ViewModels.ContactVms;
+using System.Text;
+
+namespace RentCar.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactVM createContactVM)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            createContactVM.sendDate = DateTime.Now;
+            var jsonData=JsonConvert.SerializeObject(createContactVM);
+
+            StringContent content = new StringContent(jsonData, Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7263/api/Contacts", content);
+
+            if(responseMessage.IsSuccessStatusCode)
+                return RedirectToAction("Index", "Default");
+            
+            return View();
+        }
+    }
+}
---
+++ Frontends/RentCar.Dtos/ContactVms/CreateContactVM.cs
@@ -0,0 +1,11 @@
+﻿namespace RentCar.ViewModels.ContactVms
+{
+    public class CreateContactVM
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string subject { get; set; }
+        public string message { get; set; }
+        public DateTime sendDate { get; set; }
+    }
+}
