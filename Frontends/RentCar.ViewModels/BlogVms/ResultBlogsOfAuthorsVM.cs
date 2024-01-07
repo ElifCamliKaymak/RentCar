f13357e719551a60b25dac1d2ac1256e41a0e3a7
Elif KAYMAK <camlielif@gmail.com>
2024-01-07 22:09:38 +0300

    Admin areası için yazar listesinden yazarın bloglarının listelendiği sayfa eklendi.

---
+++ Frontends/RentCar.ViewModels/BlogVms/ResultBlogsOfAuthorsVM.cs
@@ -0,0 +1,13 @@
+﻿namespace RentCar.ViewModels.BlogVms
+{
+    public class ResultBlogsOfAuthorsVM
+    {
+        public int blogId { get; set; }
+        public string title { get; set; }
+        public string coverImageUrl { get; set; }
+        public DateTime createdDate { get; set; }
+        public string description { get; set; }
+        public string authorName { get; set; }
+        public string categoryName { get; set; }
+    }
+}
---
+++ Frontends/RentCar.WebUI/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentCar.ViewModels.AuthorVms;
+using RentCar.ViewModels.BlogVms;
+
+namespace RentCar.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BlogController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBlogsOfAuthors(int authorId)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7263/api/Blogs/GetBlogsOfAuthors/{authorId}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBlogsOfAuthorsVM>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
