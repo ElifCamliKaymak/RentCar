6b1e9cc5a8ccadb2a29c0f4616b323ac9c0f2430
Elif KAYMAK <camlielif@gmail.com>
2024-01-09 20:53:07 +0300

    admin areası için iletişim kısmından gelen mesajların listelenmesi için gerekli sayfalar ve metotlar düzenlendi.

---
+++ Frontends/RentCar.WebUI/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentCar.ViewModels.ContactVms;
+
+namespace RentCar.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7263/api/Contacts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultContactVM>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
