d5ed3c4d58e4d31f337baf44c7af7ec3d3cf4d27
Elif KAYMAK <camlielif@gmail.com>
2024-01-24 12:12:00 +0300

    Footer düzenlemeleri yapıldı.

--- Frontends/RentCar.WebUI/ViewComponents/LayoutViewComponents/_FooterLayoutComponentPartial.cs
+++ Frontends/RentCar.WebUI/ViewComponents/LayoutViewComponents/_FooterLayoutComponentPartial.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentCar.ViewModels.FooterAddressVms;
 
 namespace RentCar.WebUI.ViewComponents.LayoutViewComponents
 {
     public class _FooterLayoutComponentPartial :ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterLayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7263/api/FooterAddresses");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAddressVM>>(jsonData);                
+                return View(values.LastOrDefault());
+            }
             return View();
         }
     }
