ca43654993c292191c020e4b3283f3e5c64ca1d9
Elif KAYMAK <camlielif@gmail.com>
2024-01-31 15:54:53 +0300

    araç açıklamaları iiçin UI düzenlendi.

---
+++ Frontends/RentCar.ViewModels/CarDescriptionVms/ResultCarDescriptionVM.cs
@@ -0,0 +1,9 @@
+﻿namespace RentCar.ViewModels.CarDescriptionVms
+{
+    public class ResultCarDescriptionVM
+    {
+        public int CarDescriptionId { get; set; }
+        public int CarId { get; set; }
+        public string Details { get; set; }
+    }
+}
