4adfafdfbf606050517b4fcf6fcf7c432ce33d6e
Elif KAYMAK <camlielif@gmail.com>
2024-02-06 14:23:46 +0300

    UI için login sayfası oluşturuldu.

---
+++ Frontends/RentCar.ViewModels/LoginVms/CreateLoginVM.cs
@@ -0,0 +1,8 @@
+﻿namespace RentCar.ViewModels.LoginVms
+{
+    public class CreateLoginVM
+    {
+        public string Userame { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Frontends/RentCar.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentCar.ViewModels.LoginVms;
+
+namespace RentCar.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public LoginController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateLoginVM createLoginVM)
+        {
+            //var client = _httpClientFactory.CreateClient();
+            //var jsonData = JsonConvert.SerializeObject(createLoginVM);
+
+            //StringContent content = new StringContent(jsonData);
+            //var responseMessage = await client.PostAsync()
+                return View();
+        }
+    }
+}
